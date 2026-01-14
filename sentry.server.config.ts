8e8059dbd5aaef5e53be43ed62c8b245e100eae8
shoaibakhtar800 <cs08018akp@gmail.com>
2026-01-14 10:41:31 +0530

    added logs for error tracking

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -16,5 +16,10 @@ Sentry.init({
   // Enable sending user PII (Personally Identifiable Information)
   // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
   sendDefaultPii: true,
-  integrations: [Sentry.vercelAIIntegration()],
+  integrations: [
+    Sentry.vercelAIIntegration(),
+    Sentry.consoleIntegration({
+      levels: ["log", "warn", "error"],
+    }),
+  ],
 });
